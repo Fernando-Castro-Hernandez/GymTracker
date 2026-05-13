1440fc126f3b917a67ffb5c43279633a0a53f9c2
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-05-12 20:05:04 -0600

    feat: add GrupoMuscular enum for exercise categorization

---
+++ Models/Enums/GrupoMuscular.cs
@@ -0,0 +1,13 @@
+﻿namespace GymTracker.Models.Enums
+{
+    public enum GrupoMuscular
+    {
+        Pecho,
+        Espalda,
+        Pierna,
+        Hombro,
+        Brazo,
+        Core,
+        Cardio
+    }
+}
